4fc7b85fd8d641690e774a70b8e38c58f2c2c8b4
KDJ1221 <kimmikojames@gmail.com>
2020-09-16 09:23:16 -0700

    Adding some css and javascript for game setup

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // javascript 
+const grid = document.querySelector('.grid')
+const startButton = document.querySelector('#start')
+const score = document.querySelector('#score')
+let squares = []
+let currentSnake = [0,1,2]
+
+function createGrid() {
+    for (let i=0; i < 100; i++){
+        const square = document.createElement('div')
+        console.log(square)
+        //add style to square
+        square.classList.add('square')
+        //putting squares into the div
+        grid.appendChild(square)
+
+        squares.push(square)
+    }   
+}
+
+createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
